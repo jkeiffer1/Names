d1360fa6618f5add2b614acca6beb2393594b84f
jkeiffer1 <saros82@hotmail.com>
2017-06-26 13:31:53 -0400

    fixed repeating of 'what's first name', moved line 13 to line 16, added =first_name/last_name() on lines 13 and 14

--- Names.rb
+++ Names.rb
@@ -1,14 +1,16 @@
 def first_name()
 	puts "Whats your first name?"
-first_name = gets.chomp
+	first_name = gets.chomp
  end
 
 def last_name()
 	puts "what's your last name?"
-last_name = gets.chomp
+	last_name = gets.chomp
 
-puts "Your name is #{first_name.capitalize} #{last_name.capitalize}."
+
 end
 
-first_name()
-last_name()
+first_name = first_name()
+last_name = last_name()
+
+puts "Your name is #{first_name.capitalize} #{last_name.capitalize}."
